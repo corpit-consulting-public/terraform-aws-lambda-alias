d59cbde6c5b587e758dfca4d31f0c68683bf9b9f
Circular-test <nmartinez@corpit-consulting.com>
2019-08-02 08:54:49 -0300

    New module lambda alias

--- main.tf
+++ main.tf
@@ -1,1 +1,6 @@
-
+resource "aws_lambda_alias" "test_alias" {
+  name             = "${var.name}"
+  description      = "${var.description}"
+  function_name    = "${var.function_name}"
+  function_version = "${var.function_version}"
+}
